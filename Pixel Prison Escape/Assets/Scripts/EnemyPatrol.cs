f800749cffeadd0666b5e2ce27c56ee8c9967c24
Jason <jason.castillo42@gmail.com>
2023-04-30 12:59:28 -0700

    EnemyPatroller attack on player detection

--- Pixel Prison Escape/Assets/Scripts/EnemyPatrol.cs
+++ Pixel Prison Escape/Assets/Scripts/EnemyPatrol.cs
@@ -7,28 +7,78 @@ public class EnemyPatrol : MonoBehaviour
     [SerializeField] float moveSpeed = 1f;
 
     Rigidbody2D myRigidbody;
-    BoxCollider2D myBoxCollider;
+    public triggerArea attackZone;
+
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private enum MovementState{
+        walking,
+        idle,
+        punching
+    }
+
+    public bool _hasTarget = false;
+    //detects if player is in detection zone
+    public bool HasTarget{
+        get { return _hasTarget;} 
+        private set{
+            _hasTarget = value;
+        }
+    }
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        
     }
 
+    MovementState state = MovementState.walking;
     void Update()
     {
-        if(isFacingRight()){
+        //player in attack range
+        HasTarget = attackZone.detectedColliders.Count > 0;
+
+        if(HasTarget && state!=MovementState.punching){state = MovementState.punching;}
+        else if(isFacingRight()&&state!=MovementState.punching){
+            //walk right
             myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+            state = MovementState.walking;
         }
-        else{
+        else if (!isFacingRight()&&state!=MovementState.punching){
+            //walk left
             myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+            state = MovementState.walking;
+        }
+
+        anim.SetInteger("state", (int)state);
+        if(state == MovementState.punching){
+
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collection) {
-        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), transform.localScale.y);
+
+    //finds edge of ground
+    //when ground colldider exits the enemy's detection collider, enemy is turned around
+    private void OnTriggerExit2D(Collider2D collider) {
+        //prevents player exiting the detection collider from flipping enemy
+        if(collider.gameObject.tag != "Player"){   
+            transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), transform.localScale.y);
+        }
     }
     private bool isFacingRight(){
         return transform.localScale.x > Mathf.Epsilon;
+    }
+
+    //detects the end of enemy punching animation
+    public void AlertObservers(string message)
+    {
+        if (message.Equals("AttackAnimationEnded"))
+        {
+            state = MovementState.walking;
+            // Do other things based on an attack ending.
+        }
     }
 }
 
