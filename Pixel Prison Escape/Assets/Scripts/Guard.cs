c96239b73d24daea36c74095a6a03c99cda602f2
Jason <jason.castillo42@gmail.com>
2023-05-01 22:17:33 -0700

    Guard enemy chase

--- Pixel Prison Escape/Assets/Scripts/Guard.cs
+++ Pixel Prison Escape/Assets/Scripts/Guard.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Guard : Enemy
 {
     Rigidbody2D myRigidbody;
-
+    public Transform target;
     private SpriteRenderer sprite;
     private Animator anim;
+    //public Transform home;
+    public float chaseRadius;
+    public float attackRadius;
+
     private enum MovementState{
         idle,
         walking
@@ -17,6 +21,7 @@ public class Guard : Enemy
     MovementState state = MovementState.idle;
     void Start()
     {
+        target = GameObject.FindWithTag("Player").transform;
         myRigidbody = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();        
@@ -25,6 +30,17 @@ public class Guard : Enemy
     // Update is called once per frame
     void Update()
     {
-        
+        CheckDistance();
+        anim.SetInteger("state", (int)state);
+    }
+
+    void CheckDistance(){
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius){
+            Vector3 newPos = transform.position;
+            newPos.x = Mathf.MoveTowards(transform.position.x, target.position.x, moveSpeed*Time.deltaTime);
+            transform.position = newPos;
+            state = MovementState.walking;
+        }
+        else state = MovementState.idle;
     }
 }
